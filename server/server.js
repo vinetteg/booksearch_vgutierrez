4373fce06afb4f48a8d32eb7c162b9d392980005
Vinette Gutierrez <vinette.gutierrez@gmail.com>
2021-09-09 20:10:08 -0700

    work on schemas

--- server/server.js
+++ server/server.js
@@ -1,33 +1,11 @@
-// const express = require('express');
-// const { ApolloServer } = require("apollo-server-express");
-// const path = require('path');
-// const db = require('./config/connection');
-// const routes = require('./routes');
-
-// const app = express();
-// const PORT = process.env.PORT || 3001;
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// // if we're in production, serve client/build as static assets
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// }
-
-// app.use(routes);
-
-// db.once('open', () => {
-//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-// });
-
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
-
-const { typeDefs, resolvers } = require("./schemas");
-
 const db = require("./config/connection");
+// const routes = require('./routes');
+const resolvers = require("./scemas/resolvers.js");
+const typeDefs = require("./scemas/typeDefs.js");
+const { authMiddleware } = require("./utils/auth");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -35,9 +13,8 @@ const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware,
 });
-
-server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -46,11 +23,46 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
-
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
+// app.use(routes);
+
 db.once("open", () => {
-  app.listen(PORT, () => console.log(`Now listening on localhost: ${PORT}`));
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
+// const express = require("express");
+// const { ApolloServer } = require("apollo-server-express");
+// const path = require("path");
+
+// const { typeDefs, resolvers } = require("./schemas");
+
+// const db = require("./config/connection");
+
+// const app = express();
+// const PORT = process.env.PORT || 3001;
+
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+// });
+
+// server.applyMiddleware({ app });
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// // if we're in production, serve client/build as static assets
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../client/build")));
+// }
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
+
+// db.once("open", () => {
+//   app.listen(PORT, () => console.log(`Now listening on localhost: ${PORT}`));
+// });
